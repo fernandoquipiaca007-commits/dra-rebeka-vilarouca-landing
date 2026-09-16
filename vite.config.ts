fa2d14ea2dae9ff877cd821314770f7378f2db0f
Fernando Quipiaca <fernandoquipiaca007@gmail.com>
2026-09-16 23:07:17 +0100

    Fix: Re-save package.json, tsconfig.json, vite.config.ts, index.html with clean UTF-8 without BOM

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-mport tailwindcss from '@tailwindcss/vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
